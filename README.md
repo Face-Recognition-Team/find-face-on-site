73d7b6a7e5c461e10b9d61c6cd8d12488364f099
FoKycHuK <zagnoiko@skbkontur.ru>
2017-10-17 15:25:18 +0500

    image_downloader

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # find-face-on-site
 Find your face on a websites
+
+===
+
+*image_downloader*
+usage:
+
+`import image_downloader
+image_downloader.get_images('http://e1.ru')`
+
+второй аргумент, опционально, принимает output_directory. по дефолту 'photos'
---
+++ image_downloader.py
@@ -0,0 +1,33 @@
+import re
+import requests
+import os
+from bs4 import BeautifulSoup
+
+
+def get_images(site, output_dir = 'photos'):
+    if not site.startswith('http'):
+        site = 'http://' + site
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    response = requests.get(site)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img_tags = soup.find_all('img')
+
+    urls = [img['src'] for img in img_tags]
+
+    for url in urls:
+        filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+        if not filename:
+            continue
+        with open(os.path.join(output_dir, filename.group(1)), 'wb') as f:
+            if 'http' not in url:
+                # sometimes an image source can be relative 
+                # if it is provide the base url which also happens 
+                # to be the site variable atm. 
+                url = '{}{}'.format(site, url)
+            response = requests.get(url)
+            f.write(response.content)
+
+if __name__ == '__main__':
+    get_images('e1.ru')
